1ce4061991c3b427d922fbc4293539415c1cdf91
chiaramery <meringolachiara@gmail.com>
2023-02-14 11:42:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import HomePage from './components/HomePage.vue';
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<template>
+  <HomePage />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'HomePage',
+
+}
+</script>
+
+<template>
+    <h1>Home Page</h1>
+</template>
+
+<style scoped lang="scss">
+
+</style>
