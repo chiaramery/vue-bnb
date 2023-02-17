85f3aa9babafd770342dd36d46c56fed8202a0dd
GtDavide01 <davidefiorini01@gmail.com>
2023-02-17 16:55:32 +0100

    Add : Map in ApartmentDetails

--- src/components/ApartmentMap.vue
+++ src/components/ApartmentMap.vue
@@ -5,7 +5,7 @@ export default {
   name: "ApartmentMap",
   data() {
     return {
-      url: "http://localhost:8000",
+      url: "http://localhost:8000/",
       apartment: {},
       lati: null,
       long: null,
@@ -13,91 +13,27 @@ export default {
   },
   created() {
     const slug = this.$route.params.slug;
-    axios
-      .get(`${this.url}/api/apartments/${slug}`)
-      .then((resp) => {
-        if (resp.data.success) {
-          // console.log('dentro la chiamata');
-          this.lati = resp.data.apartment.latitude;
-          this.long = resp.data.apartment.longitude;
-        }
-        // console.log(this.lati, this.long);
-      })
-      .catch((error) => {
-        if (error.response) {
-          // La risposta contiene un codice di stato diverso da 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // La richiesta è stata fatta ma non si è ricevuta risposta
-          console.log(error.request);
-        } else {
-          // Si è verificato un errore durante la richiesta
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
+    axios.get(`${this.url}api/apartments/${slug}`).then((resp) => {
+      if (resp.data.success) {
+        // console.log('dentro la chiamata');
+        this.lati = resp.data.apartment.latitude;
+        this.long = resp.data.apartment.longitude;
+        this.creteMap(this.lati, this.long);
+      }
+      // console.log(this.lati, this.long);
+    });
   },
-
-  setup() {
-    const mapRef = ref(null);
-    const dotLocation = [12.67056, 41.531];
-    onMounted(() => {
-      const tt = window.tt;
+  methods: {
+    creteMap(lati, long) {
       var map = tt.map({
         key: "upEwnVbILIY3XpQgAsiO3mhPUP6dQdCd",
-        container: mapRef.value,
+        container: "map",
         style: "tomtom://vector/1/basic-main",
-        center: dotLocation,
+        center: [long, lati],
         zoom: 15,
       });
-      var marker = new tt.Marker().setLngLat(dotLocation).addTo(map);
-      map.addControl(new tt.FullscreenControl());
-      map.addControl(new tt.NavigationControl());
-    });
-
-    return {
-      mapRef,
-    };
-    function addMarker(map) {
-      const tt = window.tt;
-      let location = [this.long, this.lati];
-      let popupOffset = 25;
-
-      let marker = new tt.Marker().setLngLat(location).addTo(map);
-      let popup = new tt.Popup({ offset: popupOffset }).setHTML(
-        "Your address!"
-      );
-      marker.setPopup(popup).togglePopup();
-    }
-    onMounted(() => {
-      // [...]
-      addMarker(map);
-    });
-    function reverseGeocoding(marker, popup) {
-      const tt = window.tt;
-      tt.services
-        .reverseGeocode({
-          key: "iTF86GRA2V5iGjM6LMMV54lrK8v6zC1w",
-          position: marker.getLngLat(),
-        })
-        .go()
-        .then(function (result) {
-          console.log(result.addresses[0].address.freeformAddress);
-          popup.setHTML(result.addresses[0].address.freeformAddress);
-        });
-    }
-    function addMarker(map) {
-      const tt = window.tt;
-      let location = [this.long, this.lati];
-      let popupOffset = 25;
-
-      let marker = new tt.Marker().setLngLat(location).addTo(map);
-      let popup = new tt.Popup({ offset: popupOffset });
-      reverseGeocoding(marker, popup);
-      marker.setPopup(popup).togglePopup();
-    }
+      var marker = new tt.Marker().setLngLat([long, lati]).addTo(map);
+    },
   },
 };
 </script>
@@ -109,6 +45,6 @@ export default {
 <style>
 #map {
   height: 50vh;
-  width: 90vw;
+  /* width: 90vw; */
 }
 </style>
