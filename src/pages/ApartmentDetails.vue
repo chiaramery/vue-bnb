d6e91b09247b5d745ac65dadeec512789a8feed0
Serenajandra <serena.jandra@gmail.com>
2023-02-17 14:22:42 +0100

    added messages form

--- src/pages/ApartmentDetails.vue
+++ src/pages/ApartmentDetails.vue
@@ -7,6 +7,11 @@ export default {
     return {
       url: "http://localhost:8000",
       apartment: {},
+      name: "",
+      email: "",
+      message: "",
+      success: false,
+      errors: {}
     };
   },
   components: {
@@ -22,6 +27,30 @@ export default {
       }
     });
   },
+  methods: {
+        sendForm() {
+            this.loading = true;
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            };
+            this.errors = {};
+
+            axios.post(`${this.store.apiBaseUrl}/api/leads`, data).then(resp => {
+                this.success = resp.data.success;
+                if (this.success) {
+                    this.name = "";
+                    this.email = "";
+                    this.message = "";
+                } else {
+                    console.log(resp.data);
+                    this.errors = resp.data.errors;
+                }
+                this.loading = false;
+            })
+        }
+    }
 };
 </script>
 <template>
@@ -73,6 +102,40 @@ export default {
       <!-- Mappa -->
       <ApartmentMap />
     </section>
+
+    <!-- Form messaggi -->
+
+    <section class="messages">
+      <h4 class="text-center m-3 text-uppercase">Contatta il proprietario:</h4>
+        
+      <div class="alert alert-success" v-if="success">
+            Il tuo messaggio è stato inviato. Ti rispondiamo a breve.
+        </div>
+        <div class="text-center" v-if="loading">Invio ...</div>
+        
+        <form @submit.prevent="sendForm()">
+            <div class="mb-3">
+                <label for="name">Nome</label>
+                <input type="text" id="name" class="form-control" :class="{ 'is-invalid': errors.name }" v-model="name">
+                <small class="invalid-feedback" v-if="errors.name">{{ errors.name[0] }}</small>
+            </div>
+    
+            <div class="mb-3">
+                <label for="email">Email</label>
+                <input type="text" id="email" class="form-control" v-model="email">
+            </div>
+    
+            <div class="mb-3">
+                <label for="message">Messaggio</label>
+                <textarea id="message" rows="10" class="form-control" v-model="message"></textarea>
+            </div>
+    
+            <button type="submit" class="btn btn-success">Invia</button>
+    
+        </form>
+    
+    </section>
+
   </div>
 </template>
 
@@ -110,6 +173,10 @@ export default {
         }
       }
     }
+  }
+  .messages{
+    width: 70%;
+    margin: 2rem auto;
   }
 }
 </style>
